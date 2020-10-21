0163dbd680c6b50be2474758608a11a77e241000
aitorf94 <72646976+aitorf94@users.noreply.github.com>
2020-10-21 22:56:31 +0200

    Update main.py
    Afegit workaround per al certificat SSL de la pagina

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from AutoSuzukiScraper import AutoSuzukiScraper
 #import importlib
 #importlib.reload(AutoSuzukiScraper)
 #output_file = "dataset.csv"
+# Sembla que utilitzant urllib el certifict de https://auto.suzuki.es no es troba
+# Les seguesnt dos linies ens deixen fer un workaround 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Tambe podriem modificar el codi i utlitzar requests.get en compte de urllib
 
 scraper = AutoSuzukiScraper();
 scraper.scrape();
